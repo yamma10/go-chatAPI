1d369f3a00236e5c8664e29d36ce97eaeff9bfa1
yamma10 <yamtt1326@gmail.com>
2023-07-14 21:09:11 +0900

    migration

---
+++ model/talkroom.go
@@ -0,0 +1,17 @@
+package model
+
+import "time"
+
+type TalkRoom struct {
+	ID uint `json:"id" gorm:"primaryKey"`
+	User1     uint     `json:"user_1" gorm:"foreignKey:User"`
+	User2     uint      `json:"user_2" gorm:"foreignKey:User"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type TalkRoomResponse struct {
+	ID uint `json:"id" gorm:"primaryKey"`
+	User1     uint      `json:"user_1" gorm:"foreignKey:User1ID"`
+	User2     uint      `json:"user_2" gorm:"foreignKey:User2ID"`
+}
